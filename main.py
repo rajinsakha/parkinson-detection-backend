39e284527fac0e8fb8340075f01d7982903764cf
Rajin Sakha <rajinsakha07@gmail.com>
2024-03-19 15:35:48 +0545

    rollback

--- main.py
+++ main.py
@@ -40,17 +40,24 @@ class modelInput(BaseModel):
 # loading the saved model
 parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
+
+
 @app.post('/parkinsons_detection')
 def parkinsons_detection(input_parameters: modelInput):
-    mdvp_fo = input_parameters.MDVP_Fo_Hz
-    mdvp_flo = input_parameters.MDVP_Flo_Hz
-    mdvp_shimmer = input_parameters.MDVP_Shimmer
-    shimmer_apq5 = input_parameters.Shimmer_APQ5
-    mdvp_apq = input_parameters.MDVP_APQ
-    hnr = input_parameters.HNR
-    sp1 = input_parameters.spread1
-    sp2 = input_parameters.spread2
-    ppe = input_parameters.PPE
+    
+    input_data = input_parameters.json()
+    input_dictionary = json.loads(input_data)
+
+    mdvp_fo = input_dictionary['MDVP_Fo_Hz']
+    mdvp_flo = input_dictionary['MDVP_Flo_Hz']
+    mdvp_shimmer = input_dictionary['MDVP_Shimmer']
+    shimmer_apq5 = input_dictionary['Shimmer_APQ5']
+    mdvp_apq = input_dictionary['MDVP_APQ']
+    hnr = input_dictionary['HNR']
+    sp1 = input_dictionary['spread1']
+    sp2 = input_dictionary['spread2']
+    ppe = input_dictionary['PPE']
+    
    
     input_list = [mdvp_fo, mdvp_flo, mdvp_shimmer, shimmer_apq5, mdvp_apq, hnr, sp1, sp2, ppe]
 
@@ -60,29 +67,3 @@ def parkinsons_detection(input_parameters: modelInput):
         return "The person doesn't have Parkinson's Disease"
     else:
         return "The person has Parkinson's Disease"
-
-# @app.post('/parkinsons_detection')
-# def parkinsons_detection(input_parameters: modelInput):
-    
-#     input_data = input_parameters.json()
-#     input_dictionary = json.loads(input_data)
-
-#     mdvp_fo = input_dictionary['MDVP_Fo_Hz']
-#     mdvp_flo = input_dictionary['MDVP_Flo_Hz']
-#     mdvp_shimmer = input_dictionary['MDVP_Shimmer']
-#     shimmer_apq5 = input_dictionary['Shimmer_APQ5']
-#     mdvp_apq = input_dictionary['MDVP_APQ']
-#     hnr = input_dictionary['HNR']
-#     sp1 = input_dictionary['spread1']
-#     sp2 = input_dictionary['spread2']
-#     ppe = input_dictionary['PPE']
-    
-   
-#     input_list = [mdvp_fo, mdvp_flo, mdvp_shimmer, shimmer_apq5, mdvp_apq, hnr, sp1, sp2, ppe]
-
-#     detection = parkinsons_model.predict([input_list])
-    
-#     if detection[0] == 0:
-#         return "The person doesn't have Parkinson's Disease"
-#     else:
-#         return "The person has Parkinson's Disease"
